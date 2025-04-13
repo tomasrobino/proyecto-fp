5ad3b87936200d2448a71f1668fee1eb4c0da955
Tomas Robino <tomaserobino@gmail.com>
2025-04-13 19:27:17 +0200

    #27 uso de metodos CRUD para Clientes por Usuario

--- programacion/src/alquilercoches/personal/Usuario.java
+++ programacion/src/alquilercoches/personal/Usuario.java
@@ -1,7 +1,32 @@
 package alquilercoches.personal;
 
-public class Usuario extends Empleado{
+import alquilercoches.Cliente;
+import alquilercoches.db.DBCRUDClientes;
+
+import java.util.List;
+
+public class Usuario extends Empleado {
     public Usuario(String dni, String nombre) {
         super(dni, nombre);
+    }
+
+    public void insertarCliente(Cliente c) {
+        DBCRUDClientes.insertarCliente(c);
+    }
+
+    public Cliente buscarCliente(String dni) {
+        return DBCRUDClientes.buscarCliente(dni);
+    }
+
+    public List<Cliente> obtenerTodosLosClientes() {
+        return DBCRUDClientes.obtenerTodosLosClientes();
+    }
+
+    public void actualizarCliente(Cliente c) {
+        DBCRUDClientes.actualizarCliente(c);
+    }
+
+    public void eliminarCliente(String dni) {
+        DBCRUDClientes.eliminarCliente(dni);
     }
 }
