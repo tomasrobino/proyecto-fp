0ae45f1c351ef3bec766ac16febc2d09e5fe7746
Tomas Robino <tomaserobino@gmail.com>
2025-04-13 04:12:32 +0200

    #38 clase Cliente

---
+++ programacion/src/alquilercoches/Cliente.java
@@ -0,0 +1,30 @@
+package alquilercoches;
+
+public class Cliente {
+    private String telefono;
+    private String dni;
+    private String nombre;
+    private String direccion;
+    private String email;
+
+    public Cliente(String dni, String nombre, String direccion, String telefono, String email) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.email = email;
+    }
+
+    public String getDni() { return dni; }
+    public String getNombre() { return nombre; }
+    public String getDireccion() { return direccion; }
+    public String getTelefono() { return telefono; }
+    public String getEmail() { return email; }
+
+    public void setDni(String dni) { this.dni = dni; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+    public void setDireccion(String direccion) { this.direccion = direccion; }
+    public void setTelefono(String telefono) { this.telefono = telefono; }
+    public void setEmail(String email) { this.email = email; }
+}
+
---
+++ programacion/src/alquilercoches/db/DBCRUDClientes.java
@@ -0,0 +1,5 @@
+package alquilercoches.db;
+
+public class DBCRUDClientes {
+
+}
