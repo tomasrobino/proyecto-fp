db7dc98874fac720eb5a58c3c7c89ddf32b96914
Tomas Robino <tomaserobino@gmail.com>
2025-04-27 05:32:01 +0200

    #13 restauro Cliente a su estado anterior

--- programacion/src/alquilercoches/negocio/Cliente.java
+++ programacion/src/alquilercoches/negocio/Cliente.java
@@ -9,7 +9,6 @@ public class Cliente {
     private String nombre;
     private String direccion;
     private String email;
-    private final List<String> vehiculosAlquilados = new ArrayList<>();
 
     public Cliente(String dni, String nombre, String direccion, String telefono, String email) {
         this.dni = dni;
@@ -24,9 +23,6 @@ public class Cliente {
     public String getDireccion() { return direccion; }
     public String getTelefono() { return telefono; }
     public String getEmail() { return email; }
-    public List<String> getVehiculosAlquilados() { return vehiculosAlquilados; }
-    public void addVehiculo(String vehiculo) { this.vehiculosAlquilados.add(vehiculo); }
-    public void removeVehiculo(String vehiculo) { this.vehiculosAlquilados.remove(vehiculo); }
 
     public void setDni(String dni) { this.dni = dni; }
     public void setNombre(String nombre) { this.nombre = nombre; }
